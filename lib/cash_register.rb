e41f0259a0c8e7e4f731bea2a525dc93528aedbd
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-30 18:36:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
-
+class CashRegister
+  
+  attr_accessor :total, :cart, :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end 
+  
+  def total
+    @total
+  end
+  
+  def add_item(item_name, item_price, item_quantity = 1)
+    item_quantity.times {self.cart << item_name}
+    self.total += item_price * item_quantity
+  end 
+  
+  def apply_discount
+    if self.discount <=0 
+      "There is no discount to apply."
+    elsif @total != 0 
+      self.total = self.total / @discount 
+    end 
+    
+  end 
+  
+  def items
+    @cart
+  end 
+  
+  def void_last_transaction
+    @cart.pop
+  end 
+  
+end
